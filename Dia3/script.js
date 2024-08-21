494064cce5ef9407ce83c5741a36875fd4a6e1b5
JhosuaCarrascal <javyjhosuarojascarrascal@gmail.com>
2024-08-21 12:53:51 -0500

    feat :sparkles: finish

---
+++ Dia3/script.js
@@ -0,0 +1,29 @@
+
+function superdigit(n,k){
+    let p = ""
+    
+    for(h =0 ; h< k; h++){
+        p+=n
+    }
+    console.log(p);
+    
+    
+    if(n.length===1){
+        return parseInt(n)
+    }
+    let suma=0;
+    for( let i=0 ; i < n.length; i++){
+        suma += parseInt(n[i]);
+    }
+    suma *= k;
+    
+    console.log("superdigitos:",suma);
+    
+    
+    return superdigit(suma.toString(), 1);
+   
+}
+let n=(prompt("ingrese un numero: "))
+let k= (prompt("ingrese el numero de repeticiones : "))
+console.log(superdigit(n,k));
+
